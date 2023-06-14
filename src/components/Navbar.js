8445b0058b8bb1dfa42394c45b40fdd9666612ec
Mona Lisa <madison0327@hotmail.com>
2023-06-13 22:05:22 -0400

    changed buttons

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,11 +3,11 @@ function Navbar({ currentPage, handlePageChange }) {
       <ul className="nav nav-tabs">
         <li className="nav-item">
           <a
-            href="#home"
-            onClick={() => handlePageChange('Home')}
-            // This is a conditional (ternary) operator that checks to see if the current page is "Home"
+            href="#resume"
+            onClick={() => handlePageChange('Resume')}
+            // This is a conditional (ternary) operator that checks to see if the current page is "Resume"
             // If it is, we set the current page to 'nav-link-active', otherwise we set it to 'nav-link'
-            className={currentPage === 'Home' ? 'nav-link active' : 'nav-link'}
+            className={currentPage === 'Resume' ? 'nav-link active' : 'nav-link'}
           >
             Home
           </a>
@@ -26,7 +26,7 @@ function Navbar({ currentPage, handlePageChange }) {
           <a
             href="#portfolio"
             onClick={() => handlePageChange('Portfolio')}
-            // Check to see if the currentPage is `Blog`, and if so we use the active link class from bootstrap. Otherwise, we set it to a normal nav-link
+            // Check to see if the currentPage is `Portfolio`, and if so we use the active link class from bootstrap. Otherwise, we set it to a normal nav-link
             className={currentPage === 'Portfolio' ? 'nav-link active' : 'nav-link'}
           >
             Portfolio
