23dc68a140715d0b11352f6560a00fbd23ff06ab
Mona Lisa <madison0327@hotmail.com>
2023-06-13 21:45:58 -0400

    added files

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import PortfolioContainer from "./components/PortfolioContainer";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
+
+
+
+const App = () => <PortfolioContainer />;
+
+
+
+export default App;
---
+++ src/components/pages/About.js
@@ -0,0 +1,31 @@
+import React from "react";
+
+export default function About() {
+  return (
+    <main className="container">
+      <div className="row">
+        <div className="col-sm-12 mt-4">
+          <h1>About Page</h1>
+          <p>
+            Hello, my name is Madison Walker, and I am an industrious, technical
+            and determined professional with proven experience in spearheading
+            all aspects of software development, delivering top-notch IT
+            services and support, and maintaining hardware and software while
+            excelling in a challenging environment. I am a seasoned professional
+            with proficiencies in leading system integration and solution
+            framing, assessing and troubleshooting technical issues, providing
+            progressive roadmaps to teams, and improving IT systems. I possess
+            robust expertise in IT engineer support, IT system management,
+            software engineering operations, client services management, issue
+            Troubleshooting & Resolution, nd project status monitoring. With
+            well-developed proficiencies, I excel at cultivating strong
+            relationships with colleagues, creating and submitting reports to
+            management, and recommending highly effective solutions. I am
+            capable of prioritizing tasks, collaborating with management, and
+            developing effective communication and organizational procedures.
+          </p>
+        </div>
+      </div>
+    </main>
+  );
+}
