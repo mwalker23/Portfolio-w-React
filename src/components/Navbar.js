407f9895407a70d6f341edb73c7c363793fd058e
Mona Lisa <madison0327@hotmail.com>
2023-06-13 23:49:40 -0400

    updated navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,18 +1,7 @@
 function Navbar({ currentPage, handlePageChange }) {
     return (
       <ul className="nav nav-tabs">
-        <li className="nav-item">
-          <a
-            href="#resume"
-            onClick={() => handlePageChange('Resume')}
-            // This is a conditional (ternary) operator that checks to see if the current page is "Resume"
-            // If it is, we set the current page to 'nav-link-active', otherwise we set it to 'nav-link'
-            className={currentPage === 'Resume' ? 'nav-link active' : 'nav-link'}
-          >
-            Resume
-          </a>
-        </li>
-        <li className="nav-item">
+           <li className="nav-item">
           <a
             href="#about"
             onClick={() => handlePageChange('About')}
@@ -40,6 +29,17 @@ function Navbar({ currentPage, handlePageChange }) {
             className={currentPage === 'Contact' ? 'nav-link active' : 'nav-link'}
           >
             Contact
+          </a>
+        </li>
+        <li className="nav-item">
+          <a
+            href="#resume"
+            onClick={() => handlePageChange('Resume')}
+            // This is a conditional (ternary) operator that checks to see if the current page is "Resume"
+            // If it is, we set the current page to 'nav-link-active', otherwise we set it to 'nav-link'
+            className={currentPage === 'Resume' ? 'nav-link active' : 'nav-link'}
+          >
+            Resume
           </a>
         </li>
       </ul>
